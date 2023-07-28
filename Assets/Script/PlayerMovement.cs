f872f47ac704261f3f89c76a39b93aa1901ee7ff
RafalOpara <86914546+RafalOpara@users.noreply.github.com>
2023-07-28 09:21:29 +0200

    climbing work with animation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -74,16 +74,22 @@ public class PlayerMovement : MonoBehaviour
 
     void ClimbLadder()
     {
+        
         if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) 
         {
             myRigidbody.gravityScale=gravityScaleAtStart;
+            myAnimator.SetBool("isClimbing", false);
             return;
+           
         }
 
-
+        
 
         Vector2 climbVelocity= new Vector2(myRigidbody.velocity.x, moveInput.y* climbSpeed);
         myRigidbody.velocity= climbVelocity;
         myRigidbody.gravityScale=0f;
+
+         bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }
